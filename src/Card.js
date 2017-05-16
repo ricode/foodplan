122496bab23161f62af78374d9ae0f3eb5b6fec8
Rico <richard.butterworth123@outlook.com>
2017-05-16 22:42:16 +0100

    Changed Card to stateless component.

--- src/Card.js
+++ src/Card.js
@@ -2,20 +2,17 @@ import React from 'react';
 import { Card, CardActions, CardTitle, CardMedia } from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 
-class FoodCard extends React.Component {
+const FoodCard = ({ title, picture }) => (
+  <Card style={{ width: '350px', margin: '10px', display: 'inline-block' }}>
+    <CardMedia
+      overlay={<CardTitle title={title} style={{ fontFamily: 'haymaker' }} />}>
+      <img src={picture} alt={title} style={{ height: '175px' }} />
+    </CardMedia>
+    <CardActions>
+      <FlatButton label="ADD TO LIST" primary={true} />
+      <FlatButton label="REMOVE" secondary={true} />
+    </CardActions>
+  </Card>
+);
 
-  render(props) {
-    return (<Card style={{ width: '350px', margin: '10px', display: 'inline-block' }}>
-      <CardMedia
-        overlay={<CardTitle title={this.props.title} style={{ fontFamily: 'haymaker' }} />}>
-        <img src={this.props.picture} alt={this.props.title} style={{ height: '175px' }} />
-      </CardMedia>
-      <CardActions>
-        <FlatButton label="ADD TO LIST" primary={true} />
-        <FlatButton label="REMOVE" secondary={true} />
-      </CardActions>
-    </Card>
-    )
-  }
-}
 export default FoodCard;
