a4d0febc81cdb33cd7e254a69b552b8d8a80965d
Rico <richard.butterworth123@outlook.com>
2017-09-14 21:50:04 +0100

    Form functionality and more...

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -7,31 +7,53 @@ import spaghetti from './images/spaghetti.js'
 import fajitas from './images/fajitas.js'
 import bangers from './images/bangers.js'
 import FoodCard from './Card'
+import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
+import Photo from 'material-ui/svg-icons/image/photo';
+import FoodMenu from 'material-ui/svg-icons/maps/restaurant-menu';
+import TextField from 'material-ui/TextField';
+import Card from 'material-ui/Card';
 
 class Dashboard extends Component {
+  exampleImageInput = {
+    cursor: 'pointer',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    width: '100%',
+    opacity: 0,
+  }
+
   render() {
     return (
-        <div className="App">
-
-          <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 style={{ marginTop: '14px' }}>FOOD PLAN</h1>
-
-          </div>
-          <br />
-          <p className="App-intro">
-            Shopping lists. fast.
+      <div className="App">
+        <link href="https://fonts.googleapis.com/css?family=Arvo" rel="stylesheet" />
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 style={{ marginTop: '14px' }}>FOOD PLAN</h1>
+        </div>
+        <p className="App-intro">
+          Shopping lists. fast.
         </p>
+        <RaisedButton  label='Add meal' labelPosition="after"
+            primary={true}
+            containerElement="label"
+            icon={<FoodMenu />}
+            />
+        <br />
+        
 
         <FoodCard title='Stir Fry' picture={stirfry} />
 
-        <FoodCard title='Spaghetti Bolognese' picture={spaghetti}/>
+        <FoodCard title='Spaghetti Bolognese' picture={spaghetti} />
 
-        <FoodCard title='Fajitas' picture={fajitas}/>
+        <FoodCard title='Fajitas' picture={fajitas} />
 
-        <FoodCard title='Sausage & mash' picture={bangers}/>
+        <FoodCard title='Sausage & mash' picture={bangers} />
 
-        </div>
+      </div>
     );
   }
 }
--- src/NewMeal.js
+++ src/NewMeal.js
@@ -3,25 +3,109 @@ import logo from './logo.svg';
 import './App.css';
 import Paper from 'material-ui/Paper';
 import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
+import Photo from 'material-ui/svg-icons/image/photo';
+
 class NewMeal extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'fuck yourself',
+      ingredient: [],
+    }
+
+  }
+
+  exampleImageInput = {
+    cursor: 'pointer',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    width: '100%',
+    opacity: 0,
+  }
+
+  handleTitleChange(e) {
+    const title = e.target.value;
+    this.setState({ title: title });
+    console.log('tit:' + this.state.title)
+  }
+
+  handleIngredientChange(e) {
+    if (e.keyCode == 13) {
+      console.log(e.target.value)
+      const ingredient = e.target.value;
+      this.setState({ ingredient: [...this.state.ingredient, ingredient] });
+      e.target.value = null;
+      console.log('ing:' + this.state.ingredient)
+    }
+  }
+
+  handleImageChange(e) {
+    const image = e.target.value;
+    this.setState({ image: image });
+    console.log('image added - i think')
+  }
+
+  showState(e) {
+    console.log(this.state)
+  }
 
   render() {
     return (
-        <div className="App">
-          <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <link href="https://fonts.googleapis.com/css?family=Arvo" rel="stylesheet" />
-            <h1 style={{ textAlign: 'center', fontFamily: 'haymaker', fontSize:'50pt', color: 'darkkhaki', display: 'inline' }}>Drop in your meal</h1>
-          </div>
-
-          <div style={{ display: 'inline-flex' }}>
-            <Paper style={{ height: 750, width: 750, marginTop: '-50px' }} zDepth={2}>
-              <TextField hintText='Title' hintStyle={{ width: '100%', fontFamily: 'arvo', align: 'center' }} style={{ margin: 10 }} inputStyle={{ textAlign: 'center', fontFamily: 'haymaker' }} />
-              <TextField hintText='Photo' hintStyle={{ width: '100%', fontFamily: 'arvo', align: 'center' }} style={{ margin: 10 }} inputStyle={{ textAlign: 'center', fontFamily: 'haymaker' }} />
-              <TextField hintText='Ingredient' hintStyle={{ width: '100%', fontFamily: 'arvo', align: 'center' }} style={{ margin: 10 }} inputStyle={{ textAlign: 'center', fontFamily: 'haymaker' }} />
-            </Paper>
-          </div>
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <link href="https://fonts.googleapis.com/css?family=Arvo" rel="stylesheet" />
+          <h1 style={{ textAlign: 'center', fontFamily: 'arvo', fontSize: '50pt', color: 'darkkhaki', display: 'inline' }}>Drop in your meal</h1>
         </div>
+
+        <div style={{ display: 'inline-flex' }}>
+          <Paper style={{ width: 450, marginTop: '-50px' }} zDepth={2}>
+            <TextField
+              name='title'
+              hintText='Title'
+              onChange={this.handleTitleChange.bind(this)}
+              style={{ display: 'block', width: '100%' }}
+              hintStyle={{ width: '100%', fontFamily: 'arvo', align: 'center' }}
+              inputStyle={{ textAlign: 'center', fontFamily: 'arvo' }} />
+
+            <FlatButton
+              label="Choose an Image"
+              labelPosition="after"
+              primary={true}
+              containerElement="label"
+              icon={<Photo />}
+            >
+              <input
+                name='image'
+                type="file"
+                onChange={this.handleImageChange.bind(this)}
+                value={this.state.image}
+                style={this.exampleImageInput} />
+
+
+            </FlatButton>
+            <TextField
+              name='ingredient'
+              hintText='Ingredient'
+              onKeyDown={this.handleIngredientChange.bind(this)}
+              style={{ display: 'block', width: '100%' }}
+              hintStyle={{ width: '100%', fontFamily: 'arvo', align: 'center' }}
+              inputStyle={{ textAlign: 'center', fontFamily: 'arvo' }} />
+
+            <RaisedButton
+              label='Create meal'
+              primary={true}
+              onClick={this.showState.bind(this)}
+              style={{ marginLeft: '30', marginRight: '30', margin: 10, display: 'block' }} />
+          </Paper>
+        </div>
+      </div>
     )
   }
 }
